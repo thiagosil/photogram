5a3deba42f2bd505a44f280fb6607c6ab83a88dc
Thiago Assis <thiagosil@gmail.com>
2016-03-10 16:07:39 +0000

    Creating initial CRUD

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,47 @@
+class PostsController < ApplicationController
+    def index
+      @posts = Post.all
+    end
+    
+    def new
+      @post = Post.new
+    end
+    
+    def create
+      @post = Post.create(post_params)
+      if @post.save
+        flash[:success] = "Your post has been created."
+        redirect_to @post
+      else
+        flash[:alert] = "Halt, you fiend! You need an image to post here!"
+        render :new
+      end
+    end
+    
+    def show
+      @post = Post.find(params[:id])
+    end
+    
+    def edit
+      @post = Post.find(params[:id])
+    end
+    
+    def update
+      @post = Post.find(params[:id])
+      @post.update(post_params)
+      if @post.save
+        flash[:success] = "Post updated hombre."
+        redirect_to(post_path(@post))
+      else
+        flash[:alert] = "Halt, you fiend! You need an image to post here!"
+        render :edit
+      end
+    end
+    
+    private
+    
+    def post_params
+      params.require(:post).permit(:caption, :image)
+    end
+    
+end
